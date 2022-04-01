a8ac6e405c9f3ee04feafa571aa37636aad38e53
Fernando15Gaspari <nando15gaspari@gmail.com>
2022-04-01 15:40:08 -0300

    Pasta match.random adicionda

---
+++ match.random/script.js
@@ -0,0 +1,20 @@
+//função que usa a Match.random para gerar numeros aleatórios.
+var lista = ['item1', 'item2', 'item3'];
+
+for(let i = 0; i < 3; i++) {
+    lista[i] = document.getElementsByTagName('h1')[i];
+};
+                    
+function pisca1() { 
+
+    lista[0].style.backgroundColor = `rgb(${Math.random() * 255} ,${Math.random() * 255} ,${Math.random() * 255})`;
+                 
+};
+function pisca2() {               
+    lista[1].style.backgroundColor = `rgb(${Math.random() * 255} ,${Math.random() * 255} ,${Math.random() * 255})`;       
+};
+
+function pisca3() {               
+    lista[2].style.backgroundColor = `rgb(${Math.random() * 255} ,${Math.random() * 255} ,${Math.random() * 255})`;       
+}; 
+         
